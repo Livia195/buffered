4011efcd1166f0ac7560d4faa52a12231a8571ac
Livia195 <livia.vieirarj@gmail.com>
2021-02-17 11:21:57 -0300

    Acesso Negado

---
+++ src/entities/Produto.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Produto {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	
+	public Produto() {
+		
+	}
+	public Produto(String name, double price, int quantity) {
+	
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public Double totalprice() {
+		return quantity*price;
+	}
+	
+
+}
